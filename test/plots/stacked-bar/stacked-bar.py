b824bf4f19a8ca0acf48fa3fb8bcfa12838de6ef
Nathan Brown <nlbrow@umich.edu>
2021-04-14 04:12:55 +0000

    Make graphs a bit larger, have input files specify Y axis title

--- test/plots/stacked-bar/stacked-bar.py
+++ test/plots/stacked-bar/stacked-bar.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import rc
 from matplotlib.figure import figaspect
 
-rc('font', **{'size': '18', 'family': 'serif', 'serif': ['Palatino']})
+rc('font', **{'size': '21', 'family': 'serif', 'serif': ['Palatino']})
 rc('text', usetex=True)
 
 
@@ -20,21 +20,28 @@ def is_number(s):
         return False
 
 
-def add_subplot(filename, ax, title, show_legend=False, logy=False):  # , loc):
+def add_subplot(filename, ax, ytitle, show_legend=False, logy=False):  # , loc):
     data = [[], {}] # [[column names], {col_name: [data for col]}]
 
+    file_ytitle = ""
     with open(filename, 'r') as data_file:
         is_first = True
+        is_second = False
         for line in data_file:
             if line[0] == '#': # ignore comments
                 continue
+            if line.strip() == "":
+                is_first, is_second = is_second, is_first # swap first and second
+                continue
             arr = line.strip().split()
             if is_first:
+                file_ytitle += line.strip() + '\n'
+            elif is_second:
                 # parse header
                 for i in arr:
                     data[0].append(i) # column names
                     data[1][i] = [] # data per column
-                is_first = False
+                is_second = False
             else:
                 for i in range(len(arr)):
                     pushed_data = arr[i]
@@ -42,6 +49,8 @@ def add_subplot(filename, ax, title, show_legend=False, logy=False):  # , loc):
                         pushed_data = float(pushed_data)
                     data[1][data[0][i]].append(pushed_data) #data[0][i] is the name for this column
 
+    if ytitle == "":
+        ytitle = file_ytitle.strip() # not overridden by command line
     if add_average:
         for i in range(len(data[0])): #for each column
             key = data[0][i]
@@ -64,8 +73,6 @@ def add_subplot(filename, ax, title, show_legend=False, logy=False):  # , loc):
     all_bars = []
     index = 0
     x = np.arange(len(data[1][data[0][0]])) #number of data rows
-    prior_index = 0
-    bottom_data=0
     for i in range(1, len(data[0])):
         y = data[1][data[0][i]]
         if i == 1:
@@ -81,12 +88,8 @@ def add_subplot(filename, ax, title, show_legend=False, logy=False):  # , loc):
         prior+=y
 
     #ax.set_xticks(x + dimw / 2, data[1][data[0][0]])
-    if dim == 1:
-        ax.set_xticks(x)
-        ax.set_xticklabels(data[1][data[0][0]], rotation=xlabel_rotation)
-    else:
-        ax.set_xticks(x*len(data[0])*dimw)
-        ax.set_xticklabels(data[1][data[0][0]], rotation=xlabel_rotation)
+    ax.set_xticks(x*len(data[0])*dimw)
+    ax.set_xticklabels(data[1][data[0][0]], rotation=xlabel_rotation)
 
     if dim == 2:
         bar_modified = all_bars[0]
@@ -99,22 +102,26 @@ def add_subplot(filename, ax, title, show_legend=False, logy=False):  # , loc):
                     va='bottom', rotation=0)  # (bar_original[i].get_height() + bar_modified[i].get_height())/2 #,fontsize=17
 
     # ax.set_xlabel('Applications')
-    ax.set_ylabel(r'Misses (\%)')
-    ax.set_ylim(ymin=-3, ymax=120)
-    if title != "":
-        ax.set_title(title)
+    ax.set_ylabel(r'\% of all BTB Misses')
+    ax.set_ylim(ymin=0, ymax=max(prior)+20)
+    if ytitle != "":
+        ax.set_ylabel(ytitle)
     if show_legend:
         ax.legend(ncol=4, columnspacing=0.5, fontsize='x-small')
     #ax.legend(ncol=3, columnspacing=0.25, title=title, loc=loc)
     ax.grid(linestyle='--', zorder=0)
 
 
-w, h = figaspect(0.3/1.05)
+w, h = figaspect(0.4/0.9)
 fig, axs = plt.subplots(1, 1, figsize=(w, h))
 filename = 'no-prefetch'
+ytitle=""
 if len(sys.argv) > 1:
     filename = sys.argv[1]
-add_subplot(filename+'.txt', axs, "", True)
+if len(sys.argv) > 2:
+    ytitle=sys.argv[2]
+print(ytitle)
+add_subplot(filename+'.txt', axs, ytitle, True)
 
 plt.axhline(0, color='gray')
 plt.tight_layout()

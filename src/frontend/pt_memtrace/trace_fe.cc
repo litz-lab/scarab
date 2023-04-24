ef2baf97ff7cc11ed2310f2f6c06bb46ee0912ae
Heiner Litz <hlitz@ucsc.edu>
2023-04-24 18:41:15 +0000

    Performance Fix for trace_fe: Perform Nop encoding/generation at initialization time once by trace_fe and then reuse that NOP, only replacing pc. Improves simulaton performance by 2.5x for XGBoost

--- src/frontend/pt_memtrace/trace_fe.cc
+++ src/frontend/pt_memtrace/trace_fe.cc
@@ -6,6 +6,7 @@
 #include "isa/isa.h"
 #include "pin/pin_lib/uop_generator.h"
 #include "pin/pin_lib/x86_decoder.h"
+#include "pin/pin_lib/pin_api_to_xed.h"
 #include "statistics.h"
 #include <iostream>
 
@@ -35,6 +36,49 @@ static uint64_t        off_path_addr[MAX_NUM_PROCS] = {0};
 static std::unordered_map<uint64_t, ctype_pin_inst> pc_to_inst;
 
 
+InstInfo trace_fe_info;
+xed_decoded_inst_t trace_fe_xed_inst_nop;
+ctype_pin_inst trace_fe_inst_nop;
+
+void generate_nop(uint64_t *pc, ctype_pin_inst *inst) {
+  xed_state_t state;
+  state.mmode = XED_MACHINE_MODE_LONG_64;
+  uint8_t buf[10];
+  xed_error_enum_t res = xed_encode_nop(buf, 1);
+  if(res != XED_ERROR_NONE) {
+    printf("Failed to encode due to %s\n", xed_error_enum_t2str(res));
+  }
+  xed_decoded_inst_zero_set_mode(&trace_fe_xed_inst_nop, &state);
+  res = xed_decode(&trace_fe_xed_inst_nop, buf, sizeof(buf));
+  if(res != XED_ERROR_NONE) {
+    printf("XED NOP decode error! %s\n", xed_error_enum_t2str(res));
+  }
+  trace_fe_info.ins = &trace_fe_xed_inst_nop;
+  trace_fe_info.pc = *pc;
+  trace_fe_info.pid = 1;
+  trace_fe_info.tid = 1;
+  trace_fe_info.target = *pc + 1;
+  trace_fe_info.static_target = 0;
+  trace_fe_info.taken = 0;
+  //trace_fe_info.unknown_type = unknown_type;
+  trace_fe_info.valid = true;
+  memset(inst, 0, sizeof(ctype_pin_inst));
+  if (FRONTEND == FE_PT)
+    pt_fill_in_dynamic_info(inst, &trace_fe_info);
+  else if (FRONTEND == FE_MEMTRACE)
+    memtrace_fill_in_dynamic_info(inst, &trace_fe_info);
+  else
+    assert(0);
+  fill_in_basic_info(inst, trace_fe_info.ins);
+  uint32_t max_op_width = add_dependency_info(inst, trace_fe_info.ins);
+  fill_in_simd_info(inst, trace_fe_info.ins, max_op_width);
+  apply_x87_bug_workaround(inst, trace_fe_info.ins);
+  fill_in_cf_info(inst, trace_fe_info.ins);
+  inst->actually_taken = false;
+  print_err_if_invalid(inst, trace_fe_info.ins);
+  (*pc)++;
+}
+
 void off_path_generate_inst(uns proc_id, uint64_t *off_path_addr, ctype_pin_inst *inst) {
   auto op_iter = pc_to_inst.find(*off_path_addr);
 
@@ -44,8 +88,9 @@ void off_path_generate_inst(uns proc_id, uint64_t *off_path_addr, ctype_pin_inst
     DEBUG(proc_id, "Generate off-path inst:%lx inst_size:%i ",inst->instruction_addr, inst->size);
   }
   else {
-    *inst = create_dummy_nop(*off_path_addr, WPNM_REASON_REDIRECT_TO_NOT_INSTRUMENTED);
-    (*off_path_addr)++;
+    *inst = trace_fe_inst_nop;
+    inst->instruction_addr      = (*off_path_addr)++;
+    inst->instruction_next_addr = *off_path_addr;
   }
 }
 
@@ -144,6 +189,8 @@ void ext_trace_init() {
       memtrace_trace_read(proc_id, &next_onpath_pi[proc_id]);
     }
   }
+  uint64_t pc = 0xCAFEBABE;
+  generate_nop(&pc, &trace_fe_inst_nop);
 }
 
 void ext_trace_done() {

6637dcb6804e111c2bcc5427c20225cb80344a55
naomi <narehman@ucsc.edu>
2024-02-15 11:32:15 -0800

    added extra flag for bp direction in oracle info that isn't overwritten on btb miss

--- src/op_info.h
+++ src/op_info.h
@@ -98,6 +98,7 @@ struct Op_Info_struct {
   Addr pred_npc;   // predicted next pc field
   Addr pred_addr;  // address used to predict branch (might be fetch_addr)
   uns8 pred;       // predicted direction of branch, set by the branch predictor
+  uns8 pred_orig;  // predicted direction of branch, not overwritten on BTB miss (for fdip)
   Flag misfetch;   // true if target address is the ONLY thing that was wrong
   Flag mispred;  // true if the direction of the branch was mispredicted and the
                  // branch should cause a recovery, set by the branch predictor

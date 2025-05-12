f5a4b98039d5cb44fa2cdac872d6024140920c3b
Naomi <naomi.rehman1008@gmail.com>
2025-05-12 21:00:24 +0000

    Add confidence debugging flag (separating conf debugging from decoupled_fe debugging

--- src/confidence/btb_miss_bp_taken_conf.cc
+++ src/confidence/btb_miss_bp_taken_conf.cc
@@ -1,6 +1,6 @@
 #include "confidence/btb_miss_bp_taken_conf.hpp"
 
-#define DEBUG(proc_id, args...) _DEBUG(proc_id, DEBUG_DECOUPLED_FE, ##args)
+#define DEBUG(proc_id, args...) _DEBUG(proc_id, DEBUG_CONF, ##args)
 
 void BTBMissBPTakenConf::per_op_update(Op* op, Conf_Off_Path_Reason& new_reason) {
   if (!CONFIDENCE_ENABLE)

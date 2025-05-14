8d0b856681d39a70a94a0cea67ed95d85630d6ab
Naomi <naomi.rehman1008@gmail.com>
2025-05-14 01:19:08 +0000

    Confidence mech: add csvs for logging miss rates and cycles since last recover type by phase.

--- src/confidence/btb_miss_bp_taken_conf.hpp
+++ src/confidence/btb_miss_bp_taken_conf.hpp
@@ -21,9 +21,17 @@ class BTBMissBPTakenConfStat : public ConfMechStatBase {
   void log_off_path_event(Op* op);
   void log_resolution(Op* op);
 
+  void log_phase_cycles(Op* op);
+
   BTBMissBPTakenConf* conf_mech;
 
   Counter cnt_total_ops;
+  // for csvs
+  typedef std::tuple<Counter, Counter, Counter, double> phase_cycles_line;
+  std::vector<phase_cycles_line> btb_miss_event_cycles;
+  std::vector<phase_cycles_line> ibtb_miss_event_cycles;
+  std::vector<phase_cycles_line> mispred_event_cycles;
+  std::vector<phase_cycles_line> misfetch_event_cycles;
 
   std::map<Counter, std::tuple<Counter, Counter, Off_Path_Reason>> resteer_ops_cycles;
   std::map<Counter, std::tuple<Counter, Counter, Off_Path_Reason>> resteer_ops_ops;

bf5d69bcd4b9ae0fabac81d364d5791768db676c
Naomi <naomi.rehman1008@gmail.com>
2025-06-05 22:41:21 +0000

    clang format

--- src/confidence/conf.cc
+++ src/confidence/conf.cc
@@ -164,7 +164,7 @@ void Conf::update(FT pushed_ft) {
 
   std::vector<Op*> ops = pushed_ft.get_ops();
   ASSERT(proc_id, !ops.empty());
-  
+
   Conf_Off_Path_Reason new_reason = REASON_CONF_NOT_IDENTIFIED;
 
   for (auto op = ops.begin(); op != ops.end() - 1; ++op) {
